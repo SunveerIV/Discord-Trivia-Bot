cc5827c54357d9b82333aa7e2bdc861f607ed231
ItsTower <120111320+SunveerIV@users.noreply.github.com>
2025-11-17 19:17:54 -0500

    Add Core Bot Logic

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
-public class Main {
-    public static void main(String[] args) {
 
+import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.jetbrains.annotations.NotNull;
+
+public class Main {
+
+    private static final String CHANNEL_NAME = "minhas_bot";
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        String token = dotenv.get("DISCORD_TOKEN");
+
+        JDA api = JDABuilder.createDefault(token).enableIntents(GatewayIntent.MESSAGE_CONTENT).build();
+
+        api.addEventListener(new ListenerAdapter() {
+            @Override
+            public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+                if (event.getAuthor().isBot()) return;
+                if (!event.getChannel().getName().equals(CHANNEL_NAME)) return;
+
+                String message = event.getMessage().getContentRaw();
+
+                if (message.startsWith("hi")) {
+                    event.getChannel().sendMessage("hello!").queue();
+                }
+            }
+        });
     }
 }
