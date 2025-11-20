b2c525fd0a93ba46f50b92d3de25512559924bef
ItsTower <120111320+SunveerIV@users.noreply.github.com>
2025-11-20 02:13:16 -0500

    Implement Question class

--- src/com/sunveer/game/Question.java
+++ src/com/sunveer/game/Question.java
@@ -1,5 +1,20 @@
 package com.sunveer.game;
 
 public class Question {
+    private String questionText;
+    private Answer[] candidateAnswers;
 
+    public Question(String questionText, Answer[] candidateAnswers) {
+        this.questionText = questionText;
+        this.candidateAnswers = candidateAnswers;
+    }
+
+    public Answer[] getCandidateAnswers() {
+        return candidateAnswers;
+    }
+
+    @Override
+    public String toString() {
+        return questionText;
+    }
 }
